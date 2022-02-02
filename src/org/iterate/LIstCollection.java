3dc5e31045e09c2540b830dee6520a0674981ddd
abdFahath <abufahath0701@gmail.com>
2022-02-02 09:24:00 -0800

    message

---
+++ src/org/iterate/LIstCollection.java
@@ -0,0 +1,5 @@
+package org.iterate;
+
+public class LIstCollection {
+
+}
---
+++ src/org/iterate/ListCollection1.java
@@ -0,0 +1,18 @@
+package org.iterate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListCollection1 {
+	public static void main(String[] args) {
+		int[]a= {10,30,20,50};
+        for (int i = 0; i < a.length; i++) {
+        	System.out.println(a[i]);
+			
+		}
+		}
+	}
+	
+
+
